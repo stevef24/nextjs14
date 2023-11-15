fefb2d1cde7a545c0c8994969e0bf7594461f292
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-11-15 12:59:05 +0000

    Add pagination and search functionality

--- nextjs-dashboard/app/ui/invoices/pagination.tsx
+++ nextjs-dashboard/app/ui/invoices/pagination.tsx
@@ -12,6 +12,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
 	const searchParams = useSearchParams();
 	const currentPage = Number(searchParams.get("page")) || 1;
 	const allPages = generatePagination(currentPage, totalPages);
+
 	const createPageURL = (pageNumber: number | string) => {
 		const params = new URLSearchParams(searchParams);
 		params.set("page", pageNumber.toString());
